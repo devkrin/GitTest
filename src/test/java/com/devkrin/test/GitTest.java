b7f7631ebc0f040307dbbaf335cde0a6d9ae1509
Krin Lai <krin-lai@qq.com>
2020-03-02 13:36:58 +0800

    修改01

--- src/test/java/com/devkrin/test/GitTest.java
+++ src/test/java/com/devkrin/test/GitTest.java
@@ -3,7 +3,9 @@ package com.devkrin.test;
 public class GitTest {
 
 	public static void main(String[] args) {
+
 		System.out.println("This is a test.");
+		System.out.println("This is another test.");
 	}
 
 }
